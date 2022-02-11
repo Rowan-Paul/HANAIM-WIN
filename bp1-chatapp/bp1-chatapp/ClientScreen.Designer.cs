f754c0fe2e9f22f599602ad29e35d221a84fd9c6
Rowan-Paul <contact@rowanpaulflynn.com>
2022-02-11 10:45:02 +0100

    Update client screen naming

--- bp1-chatapp/bp1-chatapp/ClientScreen.Designer.cs
+++ bp1-chatapp/bp1-chatapp/ClientScreen.Designer.cs
@@ -31,103 +31,103 @@ namespace bp1_chatapp
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.chatBox = new System.Windows.Forms.ListBox();
+            this.messagesInput = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.connectGroupBox = new System.Windows.Forms.GroupBox();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.ipInput = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.connectGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(568, 388);
-            this.listBox1.TabIndex = 0;
+            this.chatBox.FormattingEnabled = true;
+            this.chatBox.ItemHeight = 16;
+            this.chatBox.Location = new System.Drawing.Point(12, 12);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(568, 388);
+            this.chatBox.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 416);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(492, 22);
-            this.textBox1.TabIndex = 1;
+            this.messagesInput.Location = new System.Drawing.Point(12, 416);
+            this.messagesInput.Name = "messagesInput";
+            this.messagesInput.Size = new System.Drawing.Size(492, 22);
+            this.messagesInput.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(510, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(70, 25);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Send";
-            this.button1.UseVisualStyleBackColor = true;
+            this.sendButton.Location = new System.Drawing.Point(510, 415);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(70, 25);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(593, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(195, 122);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Connect";
+            this.connectGroupBox.Controls.Add(this.connectButton);
+            this.connectGroupBox.Controls.Add(this.ipInput);
+            this.connectGroupBox.Controls.Add(this.ipLabel);
+            this.connectGroupBox.Location = new System.Drawing.Point(593, 12);
+            this.connectGroupBox.Name = "connectGroupBox";
+            this.connectGroupBox.Size = new System.Drawing.Size(195, 122);
+            this.connectGroupBox.TabIndex = 3;
+            this.connectGroupBox.TabStop = false;
+            this.connectGroupBox.Text = "Connect";
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(8, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 19);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IP address";
+            this.ipLabel.Location = new System.Drawing.Point(8, 22);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(83, 19);
+            this.ipLabel.TabIndex = 0;
+            this.ipLabel.Text = "IP address";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(8, 44);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(179, 22);
-            this.textBox2.TabIndex = 1;
+            this.ipInput.Location = new System.Drawing.Point(8, 44);
+            this.ipInput.Name = "ipInput";
+            this.ipInput.Size = new System.Drawing.Size(179, 22);
+            this.ipInput.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(8, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(179, 33);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Connect to server";
-            this.button2.UseVisualStyleBackColor = true;
+            this.connectButton.Location = new System.Drawing.Point(8, 77);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(179, 33);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect to server";
+            this.connectButton.UseVisualStyleBackColor = true;
             // 
             // ClientScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.connectGroupBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messagesInput);
+            this.Controls.Add(this.chatBox);
             this.Name = "ClientScreen";
             this.Text = "Rchat";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.connectGroupBox.ResumeLayout(false);
+            this.connectGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox connectGroupBox;
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.TextBox ipInput;
+        private System.Windows.Forms.Button connectButton;
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox messagesInput;
+        private System.Windows.Forms.Button sendButton;
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox chatBox;
 
         #endregion
     }
