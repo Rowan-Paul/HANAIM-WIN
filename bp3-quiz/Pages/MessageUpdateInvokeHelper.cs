7f043a162ed3b1e1d277d64dfd8935a66fc327a3
Rowan-Paul <contact@rowanpaulflynn.com>
2022-03-29 16:36:55 +0200

    Add signing in to bp3

---
+++ bp3-quiz/Pages/MessageUpdateInvokeHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace bp3_quiz.Pages;
+
+public class MessageUpdateInvokeHelper
+{
+    private Action action;
+
+    public MessageUpdateInvokeHelper(Action action)
+    {
+        this.action = action;
+    }
+
+    [JSInvokable("bp3-quiz")]
+    public void UpdateMessageCaller()
+    {
+        action.Invoke();
+    }
+}
