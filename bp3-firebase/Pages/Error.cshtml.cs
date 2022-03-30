7c6ed75ac7398d30feddf3e1958d3199a94eb171
Rowan-Paul <contact@rowanpaulflynn.com>
2022-03-30 10:40:17 +0200

    Update bp3 name

--- bp3-firebase/Pages/Error.cshtml.cs
+++ bp3-firebase/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace bp3_quiz.Pages;
+namespace bp3_firebase.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
