578c3cd7127c20eca5289f5c695f55202ad4b604
Rowan-Paul <contact@rowanpaulflynn.com>
2022-04-05 10:25:00 +0200

    Add quiz

---
+++ bp3-firebase/Shared/Question.cs
@@ -0,0 +1,19 @@
+﻿namespace bp3_firebase.Shared;
+
+public class Question
+{
+    private string answer;
+    private string question;
+
+    public string GetAnswer => answer;
+
+    public string GetQuestion => question;
+
+    public Question(string question, string answer)
+    {
+        this.question = question;
+        this.answer = answer;
+    }
+    
+    
+}
